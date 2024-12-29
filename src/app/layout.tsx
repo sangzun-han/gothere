65b9deb72cf537d2aac2b283854ed33bd49f09d3
sangzun-han <hsjun1996@naver.com>
2024-12-29 19:12:32 +0900

    Fix : metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,24 @@ export const metadata: Metadata = {
   title: "gothere : 당신의 흔적이 머무는 곳",
   description: "지금 여기, 이 공간에서 글을 남겨보세요.\n 글을 남긴 공간에 가야만 글를 열어 볼 수 있습니다.",
   icons: {
-    icon: "/gothere.ico",
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon-144x144.png",
+  },
+  openGraph: {
+    type: "website",
+    title: "gothere : 당신의 흔적이 머무는 곳",
+    description: "지금 여기, 이 공간에서 글을 남겨보세요. 글을 남긴 공간에 가야만 글을 열어 볼 수 있습니다.",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    siteName: "gothere",
+    images: [
+      {
+        url: "/gothere-og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "gothere : 당신의 흔적이 머무는 곳",
+      },
+    ],
+    locale: "ko_KR",
   },
 };
 

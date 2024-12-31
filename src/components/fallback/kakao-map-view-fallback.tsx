a205dbe3b769fcf2c7f519e487cc6c25c7fbae1f
sangzun-han <hsjun1996@naver.com>
2025-01-01 00:50:44 +0900

    Fix : post design

--- src/components/fallback/kakao-map-view-fallback.tsx
+++ src/components/fallback/kakao-map-view-fallback.tsx
@@ -7,18 +7,15 @@ export default function KakaoMapViewFallback() {
 
   return (
     <Map center={fallbackCenter} style={{ width: "100%", height: "100vh" }} level={5}>
-      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-20 w-11/12 z-[99] bg-white shadow-lg rounded-lg p-4">
-        <div className="flex items-center mb-4">
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-20 w-11/12 z-[99] bg-text-primary/80 shadow-lg rounded-lg p-4">
+        <div className="flex items-center">
           <div className="w-14 h-14 rounded-full overflow-hidden bg-gray-200 mr-4" />
-          <div>
+          <div className="mb-4">
             <div className="bg-gray-200 h-5 w-24 mb-2 rounded" />
-            <div className="bg-gray-200 h-4 w-32 rounded" />
+            <div className="bg-gray-200 h-4 w-32 mb-2 rounded" />
           </div>
         </div>
-
-        <div className="bg-gray-200 h-4 w-1/3 mb-4 rounded" />
-
-        <div className="bg-gray-200 h-10 w-full rounded" />
+        <div className="bg-gray-200 h-4 w-1/2 rounded" />
       </div>
       <MyMarker latitude={fallbackCenter.lat} longitude={fallbackCenter.lng} />
       <ReturnToLocationButton onClick={() => {}} />
